f43dfcd7afab227356a4a092d3202eb5f6cab207
ArtemShd <artem.shatalov.2016@mail.ru>
2025-10-01 00:20:44 +0300

    Initial commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
+import java.util.ArrayList;
+import java.util.List;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.*;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        List<User> users = new ArrayList<>();
+        users.add(new User("Petr", "Petrov", (byte) 20));
+        users.add(new User("Ivan", "Ivanov", (byte) 30));
+        users.add(new User("Vladimir", "Vladimirov", (byte) 35));
+        users.add(new User("Egor", "Egorov", (byte) 40));
+        for (User us : users) {
+            userService.saveUser(us.getName(), us.getLastName(), (byte) us.getAge());
+            System.out.println("User с именем – " + us.getName() + " добавлен в БД");
+        }
+        List<User> usersTable = userService.getAllUsers();
+        for (User us : usersTable) {
+            System.out.println(us);
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mybdtest";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Temka2323";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Подключение к БД установлено");
+        } catch (SQLException e) {
+            System.out.println("Подключение к БД НЕ установлено");
+        }
+        return connection;
+    }
 }
